1379774d231b04ef66a60d0bd56e544eb9cf4743
Lucas Leandro <lucasjleandro@gmail.com>
2022-09-02 11:09:47 -0300

    Concluindo Curso.

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LikeWidgetComponent } from './shared/components/like-widget/like-widget.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { LikeWidgetModule } from './shared/components/like-widget/like-widget.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LikeWidgetModule
   ],
   providers: [],
   bootstrap: [AppComponent]
